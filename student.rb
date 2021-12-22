0765e03ab2b55f7c96db75131c3455fabfa0d35a
Lilian Francis <ifnotlily@yahoo.com>
2021-12-22 18:57:49 +0100

    Add ruby files for book and rentals

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.students.include?(self)
   end
 end
